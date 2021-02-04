3744d0a037c5205dcd235daad1608133808a6e7f
z0ned-out <77028238+z0ned-out@users.noreply.github.com>
2021-02-05 03:54:02 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+# To avoid Reddit's rate limit. 3 is ideal.
+wait_time = 3
+
+# Your Discord webhook settings.
+webhook_url = "YOUR DISCORD WEBHOOK URL HERE"
+
+# Max post limit, 10 is recommended
+# Will run the script and fetch 10 latest posts from the given type (hot, top, new, rising, etc) from the specified subreddit(s), with a pause of 3 seconds per post.
+limit = 10
+
+# Add subreddits to track. You can use as much as subreddit you want.
+subreddits = ['subreddit1', 'subreddit2']
+
+
+# Reddit information
+username = "YOUR REDDIT USERNAME HERE"
+password = "YOUR REDDIT PASSWORD HERE"
+client_id = "YOUR REDDIT CLIENT ID HERE"
+client_secret = "YOUR REDDIT CLIENT SECRET HERE"
+user_agent = "CHANGE THIS TO WHATEVER YOU WANT!"
